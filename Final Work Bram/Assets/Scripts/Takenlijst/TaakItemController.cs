a2004b23f084b1c53400967eac2574000172ae19
BramEylenbosch <91201019+BramEylenbosch@users.noreply.github.com>
2025-06-13 12:06:42 +0200

    Fixed task list, working properly

--- Final Work Bram/Assets/Scripts/Takenlijst/TaakItemController.cs
+++ Final Work Bram/Assets/Scripts/Takenlijst/TaakItemController.cs
@@ -1,17 +1,20 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class TaakItemController : MonoBehaviour
 {
     public TextMeshProUGUI taakText;
-    public TextMeshProUGUI deadlineText; // Alleen tonen, geen inputfield!
+    public TextMeshProUGUI deadlineText; 
     public Toggle voltooidToggle;
     public Button deleteButton;
 
-    private System.Action<TaakItemController> onDelete;
+    private Action<TaakItemController> onDelete;
 
-    public void Setup(string tekst, string deadline, System.Action<TaakItemController> onDelete)
+    public event Action<bool> onVoltooidChanged;
+
+    public void Setup(string tekst, string deadline, Action<TaakItemController> onDelete)
     {
         taakText.text = tekst;
         deadlineText.text = string.IsNullOrEmpty(deadline) ? "" : $"Deadline: {deadline}";
@@ -22,11 +25,21 @@ public class TaakItemController : MonoBehaviour
 
         voltooidToggle.onValueChanged.RemoveAllListeners();
         voltooidToggle.onValueChanged.AddListener(OnToggleChanged);
+
+        // Init style
+        OnToggleChanged(voltooidToggle.isOn);
     }
 
     private void OnToggleChanged(bool isAan)
     {
         taakText.fontStyle = isAan ? FontStyles.Strikethrough : FontStyles.Normal;
         taakText.color = isAan ? Color.gray : Color.black;
+
+        onVoltooidChanged?.Invoke(isAan);
+    }
+
+    public void SetVoltooid(bool isVoltooid)
+    {
+        voltooidToggle.isOn = isVoltooid;
     }
 }
--- Final Work Bram/Assets/Scripts/Takenlijst/TakenlijstManager.cs
+++ Final Work Bram/Assets/Scripts/Takenlijst/TakenlijstManager.cs
@@ -4,12 +4,33 @@ using TMPro;
 
 public class TaaklijstManager : MonoBehaviour
 {
+    [Header("UI elementen")]
     public Transform taakContainer;
     public GameObject taakItemPrefab;
     public TMP_InputField taakInputField;
     public TMP_InputField deadlineInputField;
 
     private List<TaakItemController> taakItems = new();
+    private List<TaakData> takenLijst = new();
+
+    [System.Serializable]
+    public class TaakData
+    {
+        public string tekst;
+        public string deadline;
+        public bool voltooid;
+    }
+
+    [System.Serializable]
+    private class TaakDataWrapper
+    {
+        public List<TaakData> taken;
+    }
+
+    private void Start()
+    {
+        LaadTaken();
+    }
 
     public void VoegTaakToe()
     {
@@ -18,18 +39,81 @@ public class TaaklijstManager : MonoBehaviour
 
         if (string.IsNullOrEmpty(nieuweTaak)) return;
 
-        GameObject taakGO = Instantiate(taakItemPrefab, taakContainer);
-        TaakItemController taakItem = taakGO.GetComponent<TaakItemController>();
-        taakItem.Setup(nieuweTaak, deadline, VerwijderTaak);
-        taakItems.Add(taakItem);
+        TaakData nieuweTaakData = new TaakData
+        {
+            tekst = nieuweTaak,
+            deadline = deadline,
+            voltooid = false
+        };
+
+        takenLijst.Add(nieuweTaakData);
+        MaakTaakItem(nieuweTaakData);
 
         taakInputField.text = "";
         deadlineInputField.text = "";
+
+        SlaTakenOp();
+    }
+
+    private void MaakTaakItem(TaakData taakData)
+    {
+        GameObject taakGO = Instantiate(taakItemPrefab, taakContainer);
+        TaakItemController taakItem = taakGO.GetComponent<TaakItemController>();
+        taakItem.Setup(taakData.tekst, taakData.deadline, VerwijderTaak);
+        taakItem.SetVoltooid(taakData.voltooid);
+        taakItem.onVoltooidChanged += (isVoltooid) =>
+        {
+            taakData.voltooid = isVoltooid;
+            SlaTakenOp();
+        };
+        taakItems.Add(taakItem);
     }
 
     private void VerwijderTaak(TaakItemController taakItem)
     {
-        taakItems.Remove(taakItem);
-        Destroy(taakItem.gameObject);
+        int index = taakItems.IndexOf(taakItem);
+        if (index >= 0)
+        {
+            taakItems.RemoveAt(index);
+            takenLijst.RemoveAt(index);
+            Destroy(taakItem.gameObject);
+            SlaTakenOp();
+        }
     }
+
+    public void SlaTakenOp()
+    {
+        TaakDataWrapper wrapper = new TaakDataWrapper { taken = takenLijst };
+        string json = JsonUtility.ToJson(wrapper);
+        PlayerPrefs.SetString("takenlijst", json);
+        PlayerPrefs.Save();
+    }
+
+    public void LaadTaken()
+    {
+        // Eerst alle oude taakitems verwijderen uit de UI
+        foreach (var taakItem in taakItems)
+        {
+            Destroy(taakItem.gameObject);
+        }
+        taakItems.Clear();
+        takenLijst.Clear();
+
+        if (PlayerPrefs.HasKey("takenlijst"))
+        {
+            string json = PlayerPrefs.GetString("takenlijst");
+            TaakDataWrapper wrapper = JsonUtility.FromJson<TaakDataWrapper>(json);
+
+            if (wrapper.taken != null)
+            {
+                foreach (TaakData taak in wrapper.taken)
+                {
+                    takenLijst.Add(taak);
+                    MaakTaakItem(taak);
+                }
+            }
+        }
+    }
+
+
 }
